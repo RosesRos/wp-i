a36970d99be26df49f64d3c88b4d060fab7a60a1
RosesRos <rostranespinoza@gmail.com>
2023-08-21 16:45:59 +0300

    update

--- core/inputs.sh
+++ core/inputs.sh
@@ -8,8 +8,8 @@ input () {
 }
 
 user_db () {
-  while read -p "$1: $(c_purple)" user_db; do
-      if [ $user_db ]; then
+  while read -p "$1: $(c_purple)" USER_NAME; do
+      if [ $USER_NAME ]; then
           break;c_clean
       else echo "$(c_red) $1 should not be empty";c_clean;
       fi
