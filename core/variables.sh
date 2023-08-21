56605c25132b385047455b32601141f96bd84353
RosesRos <rostranespinoza@gmail.com>
2023-08-21 18:22:35 +0300

    update

--- core/variables.sh
+++ core/variables.sh
@@ -1,5 +1,5 @@
 ## config wp
 export wp_db_name="${HOST_NAME}1"
-export wp_html_path="/home/fabio/Downloads/$HOST_NAME"
+export wp_html_path="/var/www/$HOST_NAME"
 export wp_user_admin="${USER_ADMIN}"
 export wp_pass_admin="${PASS_ADMIN}"
