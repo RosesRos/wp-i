5204658e7277051f4dd5de6e115943e753907f27
root <root@ubuntu-s-2vcpu-4gb-intel-fra1-01>
2023-08-21 15:37:59 +0000

    update

--- core/variables.sh
+++ core/variables.sh
@@ -1,6 +1,6 @@
 ## config wp
 export wp_db_name="${HOST_NAME}1"
-export wp_html_path="/var/www/$HOST_NAME"
-# export wp_html_path="/home/admin/web/finanse-me.com/public_html$HOST_NAME"
+# export wp_html_path="/var/www/$HOST_NAME"
+export wp_html_path="/home/admin/web/finanse-me.com/public_html/$HOST_NAME"
 export wp_user_admin="${USER_ADMIN}"
 export wp_pass_admin="${PASS_ADMIN}"
